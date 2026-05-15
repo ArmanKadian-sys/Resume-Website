16872fb3657dfc1610fc72485006090bf790ad7b
ArmanKadian-sys <armankadian11@gmail.com>
2026-05-15 11:34:26 +1000

    made change in import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
 import EducationContent from "./components/EducationContent";
 import HomeContent from "./components/HomeContent";
